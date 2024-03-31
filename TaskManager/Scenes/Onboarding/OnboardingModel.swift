d302c0080da060d0b4e74a8ee6ec979d2982e3da
Ibrahim Nasser <thenasserr@gmail.com>
2024-03-31 05:58:44 +0300

    create onboarding model and view model

---
+++ TaskManager/Scenes/Onboarding/OnboardingModel.swift
@@ -0,0 +1,15 @@
+//
+//  OnboardingModel.swift
+//  TaskManager
+//
+//  Created by Ibrahim Nasser Ibrahim on 31/03/2024.
+//
+
+import Foundation
+
+struct Onboarding: Codable {
+    let id: Int
+    let image: String
+    let title: String
+    let subtitle: String
+}
---
+++ TaskManager/Scenes/Onboarding/OnboardingViewModel.swift
@@ -0,0 +1,37 @@
+//
+//  OnboardingViewModel.swift
+//  TaskManager
+//
+//  Created by Ibrahim Nasser Ibrahim on 31/03/2024.
+//
+
+import Foundation
+import Combine
+
+class OnboardingViewModel {
+    
+//    let coordinator: OnboardingCoordinatorProtocol
+//    init(coordinator: OnboardingCoordinatorProtocol, currentIndex: Int = 0) {
+//        self.coordinator = coordinator
+//    }
+    
+    var onboardingArray: [Onboarding] {
+        JSONDecoder().decode(forResource: "OnboardingData") ?? []
+    }
+    
+    var currentIndex = 0
+    
+    func move(didMoved: (Int) -> Void, reachedEnd: () -> Void) {
+        if currentIndex < onboardingArray.count - 1 {
+            currentIndex += 1
+            didMoved(currentIndex)
+        } else {
+            reachedEnd()
+//            self.showSplashScreen()
+        }
+    }
+    
+//    func showSplashScreen() {
+//        coordinator.showAuthFlow()
+//    }
+}
